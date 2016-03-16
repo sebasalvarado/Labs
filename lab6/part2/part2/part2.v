99502e7d60e1ce99c72f2c13e44811422fac9d2f
Sebastian Alvarado <sebastian.alvarado@mail.utoronto.ca>
2016-03-16 16:42:46 -0400

    Update part2.v

--- lab6/part2/part2/part2.v
+++ lab6/part2/part2/part2.v
@@ -22,7 +22,7 @@ module part2
 	input   [9:0]   SW;
 	input   [3:0]   KEY;
 	// Declare your inputs and outputs here
-	
+
 	// Do not change the following outputs
 	output			VGA_CLK;   				//	VGA Clock
 	output			VGA_HS;					//	VGA H_SYNC
@@ -32,7 +32,7 @@ module part2
 	output	[9:0]	VGA_R;   				//	VGA Red[9:0]
 	output	[9:0]	VGA_G;	 				//	VGA Green[9:0]
 	output	[9:0]	VGA_B;   				//	VGA Blue[9:0]
-	
+
 	// Create the colour, x, y and writeEn wires that are inputs to the controller.
 	wire [2:0] colour;
 	wire [7:0] x;
@@ -42,7 +42,7 @@ module part2
 	//Assign corresponding keys into the wires.
 	assign resetn = KEY[0];
 	assign colour = SW[9:7];
-	assign  go = ~KEY[1]; 
+	assign  go = ~KEY[1];
 	assign INPUT = SW[6:0];
 
 	// ALl the wires that we need to connect control and datapath
@@ -70,10 +70,10 @@ module part2
 		defparam VGA.MONOCHROME = "FALSE";
 		defparam VGA.BITS_PER_COLOUR_CHANNEL = 1;
 		defparam VGA.BACKGROUND_IMAGE = "black.mif";
-			
+
 	// Put your code here. Your code should produce signals x,y,colour and writeEn/plot
 	// for the VGA controller, in addition to any other functionality your design may require.
-    
+
     // Instansiate datapath
 	datapath d0(
 		.clk(CLOCK_50),
@@ -84,9 +84,9 @@ module part2
 		.load_r(load_r),
 		.load_c(load_c),
 		.ld_alu_out(ld_alu_out),
-		
+
 		.data_in(INPUT),
-		
+
 		.x(x),
 		.y(y)
 		);
@@ -96,6 +96,7 @@ module part2
 		.clk(CLOCK_50),
 		.resetn(resetn),
 		.go(go),
+
 		// Outputs that are being put into the datapath and the VGA
 		.load_x(load_x),
 		.load_y(load_y),
@@ -104,34 +105,34 @@ module part2
 		.load_alu_out(ld_alu_out),
 		.plot(writeEn)
 		);
-    
+
 endmodule
 
 module datapah(input clk,
-	 	input resetn, 
-		input load_x, 
-		input load_y, 
-		input load_r, 
+	 	input resetn,
+		input load_x,
+		input load_y,
+		input load_r,
 		input load_c,
-		input ld_alu_out, 
+		input ld_alu_out,
 		input [6:0] data_in,
-		output reg [7:0]x, output [6:0]y);
-	// Declare the registers that we will have 
+		output reg [7:0]x, output reg [6:0]y);
+	// Declare the registers that we will have
 	reg [7:0]x_reg;
 	reg [6:0] y_reg;
-	reg [3:0] counter; 
+	reg [3:0] counter;
 	reg [14:0] alu_output;
 
 	//Input Logic for Load registers
 	always@(posedge clk)
-	begin: InputLogic 
-		if(!resetn) begin 
+	begin: InputLogic
+		if(!resetn) begin
 			x_reg <= 8'b0;
 			y_reg <= 7'b0;
 			alu_output <= 15'b0;
 			counter <= 4'b0;
 			end
-		else begin 
+		else begin
 		     if(load_x)
 		    // Set x depending on the signal of ldu_alu_out, first 8 bits of the alu_out
 			x_reg <= ld_alu_out? {1'b0,data_in}: alu_output[14:7];
@@ -139,8 +140,8 @@ module datapah(input clk,
 			y_reg <= ld_alu_out? data_in: alu_output[6:0];
 		end
 	end
-	
-	//Output Result REgister
+
+	//Output Result Register
 	always@(posedge clk)
 	begin:Output
 		if(!resetn) begin
@@ -148,26 +149,29 @@ module datapah(input clk,
 			y <= 8'b0;
 		end
 		else
-		   begin 
+		   begin
 		   if(load_r)
 			x <=  alu_output[14:7];	// Set the output x to the first 8 bits
 			y <=  alu_output[6:0]; // Set the output y to the first 7 bits
-	           end 
+	           end
 	end
-	
+
 	//LOgic for the Counter register
 	always@(posedge clk)
 	begin:COUNTERLOGIC
-		if(counter == 4'd15) begin // the reset case is already being handled before  
+		if(counter == 4'd15) begin // the reset case is already being handled before
 			counter <= 4'b00; // When we reach 15 we reset it again and keep counting
 		end
-		else 
+		else begin
+		    if(load_c == 1'b1)begin
 		   counter <= counter + 1'b1; // add one on every clock edge
+		   end
 	end
-	
+
 	// The ALU Implementation
 	always@(*)
 	begin: ALU
+		// We add the two most significant bits to Y and the least to X
 		alu_output <= {x_reg + counter[3:2], y_reg + counter[1:0]};
 	end
 endmodule
@@ -189,8 +193,8 @@ module control(input clk,
 		   DISPLAY_RESULT = 3'd4;
 
 	reg [2:0] current_state, next_state;
-	
-	// Create the always block for the enxt state logic
+
+	// Create the always block for the next state logic
 	always@(*)
 	begin: state_table
 	     case(current_state)
@@ -204,30 +208,30 @@ module control(input clk,
 	end // THis was the state table
 
 
-	//Output logic, everything that the data path will receive as input goes here. 
+	//Output logic, everything that the data path will receive as input goes here.
 	always@(*)
 	begin: OutLogic
 	// Set all of them to zero
-		load_x = 1'b0;
-		load_y = 1'b0;
-		load_r = 1'b0;
-		load_c = 1'b0;
-		load_alu_out = 1'b0;
-		case(current_state)
-			LOAD_X: begin
-				load_x = 1'b1;
-			end
-			LOAD_Y: begin
-				load_y = 1'b1;
-			end
-			DISPLAY_RESULT: begin
-				load_x = 1'b1;
-				load_y = 1'b1;
-				load_r = 1'b1;
-				load_c = 1'b1;
-				load_alu_out = 1'b1;
-			end
-		endcase
+	   load_x = 1'b0;
+	   load_y = 1'b0;
+       	   load_r = 1'b0;
+	   load_c = 1'b0;
+	   load_alu_out = 1'b0;
+	   case(current_state)
+		LOAD_X: begin
+		  load_x = 1'b1; // send signal to allow X to be loaded
+		  end
+		LOAD_Y: begin
+		  load_y = 1'b1; // send signal to allow Y to be loaded
+		   end
+		DISPLAY_RESULT: begin
+		  load_x = 1'b1; // When we will perform the computation
+		  load_y = 1'b1; // All of the registers must be open and ready to be loaded.
+		  load_r = 1'b1;
+		  load_c = 1'b1;
+		  load_alu_out = 1'b1;
+		   end
+	   endcase
 	end
 
 	//CUrrent state registers logic on positive clock edge
@@ -235,7 +239,7 @@ module control(input clk,
 	begin: StateFFs
 		if(!resetn)
 			current_state <= LOAD_X;
-		else 
+		else
 			current_state <= next_state;
-	end // THis is the state FFs that we will use 
+	end // THis is the state FFs that we will use
 endmodule
